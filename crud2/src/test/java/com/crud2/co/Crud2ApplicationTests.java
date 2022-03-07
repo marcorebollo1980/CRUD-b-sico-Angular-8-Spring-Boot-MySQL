7b86656c1c19d62e6b0dbc27acd33053196dc61e
marcorebollo1980 <marcorebollolopez@gmail.com>
2022-03-07 17:44:49 -0500

    primer commit
    primer commit

---
+++ crud2/src/test/java/com/crud2/co/Crud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud2.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
